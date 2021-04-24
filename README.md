e081491743670e496e8141d71e3a3310a4cce182
Yan Zhang <yanzhang0219@gmail.com>
2021-04-23 20:42:29 -0700

    New Problem Solution - "2. Add Two Numbers"

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 | No. | Problem Title | Difficulty | Solution |
 |-----|---------------|------------|----------|
 | 1 | [Two Sum](https://leetcode.com/problems/two-sum/) | Easy | [java](./algorithms/java/1-Two-Sum/TwoSum.java) |
+| 2 | [Add Two Numbers](https://leetcode.com/problems/add-two-numbers/) | Medium | [java](./algorithms/java/2-Add-Two-Numbers/AddTwoNumbers.java) |
---
+++ algorithms/java/2-Add-Two-Numbers/AddTwoNumbers.java
@@ -0,0 +1,33 @@
+// Link: https://leetcode.com/problems/add-two-numbers/
+
+class Solution {
+  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    ListNode dummy = new ListNode(-1);
+    ListNode cur = dummy;
+    int carry = 0;
+    while (l1 != null || l2 != null) {
+      int n1 = l1 == null ? 0 : l1.val;
+      int n2 = l2 == null ? 0 : l2.val;
+      int sum = n1 + n2 + carry;
+      cur.next = new ListNode(sum % 10);
+      carry = sum / 10;
+      cur = cur.next;
+      if (l1 != null) {
+        l1 = l1.next;
+      }
+      if (l2 != null) {
+        l2 = l2.next;
+      }
+    }
+    if (carry != 0) {
+      cur.next = new ListNode(carry);
+    }
+    return dummy.next;
+  }
+}
+
+/*
+ * Notes:
+ * After traversing both linked lists, don't forget the last digit should be the carry if it is not zero.
+ */
+
